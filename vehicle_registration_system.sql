9c2d13580181c4a05e06e9a4374879dee03e033b
larruuhh5 <160387688+larruuhh5@users.noreply.github.com>
2025-10-29 17:08:39 +0800

    Update vehicle_registration_system.sql

--- vehicle_registration_system.sql
+++ vehicle_registration_system.sql
@@ -21,16 +21,16 @@ CREATE TABLE Branch (
 );
 
 INSERT INTO Branch VALUES
-('1', 'LTO Paranaque District Office', 'Olivarez Plaza', 'San Dionisio', 'Paranaque', 'Metro Manila', '1700', 'NCR'),
-('2', 'LTO Las Pinas District Office', 'Alabang Zapote', 'Talon Uno', 'Las Pinas', 'Metro Manila', '1747', 'NCR'),
-('3', 'LTO San Juan District Office', 'North Domingo', 'Rivera', 'San Juan', 'Metro Manila', '1500', 'NCR'),
-('4', 'LTO Pateros Extension Office', 'M. Amelda', 'San Roque', 'Pateros', 'Metro Manila', '1620', 'NCR'),
-('5', 'LTO Quezon City District Office', 'East Ave.', 'Pinyahan', 'Quezon City', 'Metro Manila', '1100', 'NCR'),
-('6', 'LTO Taguig Extension Office', 'Radiant St.', 'Western Bicutan', 'Taguig', 'Metro Manila', '1630', 'NCR'),
-('7', 'LTO Cebu City District Office', 'General Maxilom Ave.', 'Carreta', 'Cebu City', 'Cebu', '6000', 'Region VII'),
-('8', 'LTO Mandaue City District Office', 'Fortuna St.', 'Bakilid', 'Mandaue', 'Cebu', '6014', 'Region VII'),
-('9', 'LTO Baguio Office', 'Polo Field', 'Pacdal', 'Baguio', 'Benguet', '2600', 'CAR'),
-('10', 'LTO Regional Office XII - Koronadal City', 'Yellowbell', 'Sta. Cruz', 'Koronadal', 'South Cotabato', '9506', 'Region XII');
+('1001', 'LTO Paranaque District Office', 'Olivarez Plaza', 'San Dionisio', 'Paranaque', 'Metro Manila', '1700', 'NCR'),
+('1002', 'LTO Las Pinas District Office', 'Alabang Zapote', 'Talon Uno', 'Las Pinas', 'Metro Manila', '1747', 'NCR'),
+('1003', 'LTO San Juan District Office', 'North Domingo', 'Rivera', 'San Juan', 'Metro Manila', '1500', 'NCR'),
+('1004', 'LTO Pateros Extension Office', 'M. Amelda', 'San Roque', 'Pateros', 'Metro Manila', '1620', 'NCR'),
+('1005', 'LTO Quezon City District Office', 'East Ave.', 'Pinyahan', 'Quezon City', 'Metro Manila', '1100', 'NCR'),
+('1006', 'LTO Taguig Extension Office', 'Radiant St.', 'Western Bicutan', 'Taguig', 'Metro Manila', '1630', 'NCR'),
+('1007', 'LTO Cebu City District Office', 'General Maxilom Ave.', 'Carreta', 'Cebu City', 'Cebu', '6000', 'Region VII'),
+('1008', 'LTO Mandaue City District Office', 'Fortuna St.', 'Bakilid', 'Mandaue', 'Cebu', '6014', 'Region VII'),
+('1009', 'LTO Baguio Office', 'Polo Field', 'Pacdal', 'Baguio', 'Benguet', '2600', 'CAR'),
+('1010', 'LTO Regional Office XII - Koronadal City', 'Yellowbell', 'Sta. Cruz', 'Koronadal', 'South Cotabato', '9506', 'Region XII');
 
 
 /*(JP AND AYA) create registration table with at least 10 records (refer to EERD) */
@@ -57,16 +57,16 @@ CREATE TABLE Payment (
 
 INSERT INTO Payment
 (violation_id, officer_id, branch_id, amount_paid, date_paid, receipt_number, status) VALUES
-(1, 4, 3, 5000.00, '2025-07-08', 'R003', 'Cleared'),
-(2, 4, 3, 3000.00, '2025-07-20', 'R004', 'Cleared'),
-(3, 2, 1, 3000.00, '2025-08-03', 'R005', 'Cleared'),
-(3, 2, 1, 1000.00, '2025-08-04', 'R006', 'Cleared'),
-(4, 3, 2, 2000.00, '2025-08-10', 'R007', 'Cleared'),
-(5, 5, 3, 3000.00, '2025-09-12', 'R008', 'Cleared'),
-(5, 5, 3, 2000.00, '2025-09-14', 'R009', 'Cleared'),
-(1, 1, 1, 10000.00, '2025-09-20', 'R010', 'Cleared'),
-(2, 4, 3, 1000.00, '2025-10-05', 'R011', 'Cleared'),
-(4, 3, 2, 2000.00, '2025-10-08', 'R012', 'Cleared');
+(0001, 4, 1003, 5000.00, '2025-07-08', 'R003', 'Cleared'),
+(0002, 4, 1003, 3000.00, '2025-07-20', 'R004', 'Cleared'),
+(0213, 2, 1001, 3000.00, '2025-08-03', 'R005', 'Cleared'),
+(0204, 2, 1001, 1000.00, '2025-08-04', 'R006', 'Cleared'),
+(0008, 3, 1002, 2000.00, '2025-08-10', 'R007', 'Cleared'),
+(0067, 5, 1005, 3000.00, '2025-09-12', 'R008', 'Cleared'),
+(0192, 5, 1009, 2000.00, '2025-09-14', 'R009', 'Cleared'),
+(0109, 1, 1002, 10000.00, '2025-09-20', 'R010', 'Cleared'),
+(0490, 4, 1005, 1000.00, '2025-10-05', 'R011', 'Cleared'),
+(0004, 3, 1003, 2000.00, '2025-10-08', 'R012', 'Cleared');
 
 
 
