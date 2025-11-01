626c5922833b34416abecdbc6f9cac20283a6dab
larruuhh5 <160387688+larruuhh5@users.noreply.github.com>
2025-11-01 21:34:43 +0800

    Update DatabaseConnection.java

--- DBApp/src/database/DatabaseConnection.java
+++ DBApp/src/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/vehicle_registration_system";
     private static final String USER = "root";  // MySQL username
-    private static final String PASSWORD = " "; // change to ur own password 
+    private static final String PASSWORD = "Bananabread123."; // MySQL password
 
     private static Connection connection = null;
 
