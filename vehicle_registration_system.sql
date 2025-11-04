50bd0ccd14aba898919b8568b93ea09203868924
larruuhh5 <160387688+larruuhh5@users.noreply.github.com>
2025-11-04 20:32:51 +0800

    updated payment id

--- vehicle_registration_system.sql
+++ vehicle_registration_system.sql
@@ -316,12 +316,12 @@ INSERT INTO Renewal VALUES /*INCOMPLETE FIELDS*/
 (20013, 10017, 18, 1009, 11100009, '2024-11-11'),
 
 INSERT INTO Violation VALUES
-(123450, 0001, 11100003, 1003, 'Expired Registration', 3000.00, '2025-08-01', 'Cleared', 28),
+(123450, 0001, 11100003, 1003, 'Expired Registration', 3000.00, '2025-08-01', 'Cleared', 27),
 (123450, 0001, 11100003, 1003, 'Reckless Driving', 2000.00, '2025-11-17', 'Unpaid', NULL),
-(123455, 0006, 11100006, 1006, 'Unregistered Motor Vehicle', 10000.00, '2025-07-28', 'Cleared', 25),
-(123456, 0007, 11100009, 1009, 'Smoke Belching', 2000.00, '2024-03-21', 'Cleared', 9),
+(123455, 0006, 11100006, 1006, 'Unregistered Motor Vehicle', 10000.00, '2025-07-28', 'Cleared', 26),
+(123456, 0007, 11100009, 1009, 'Smoke Belching', 2000.00, '2024-03-21', 'Cleared', 10),
 (123458, 0009, 11100005, 1005, 'Unauthorized Modification', 5000.00, '2025-10-01', 'Unpaid', NULL),
-(123455, 0006, 11100006, 1006, 'Defective Parts', 5000.00, '2025-07-07', 'Cleared', 24),
+(123455, 0006, 11100006, 1006, 'Defective Parts', 5000.00, '2025-07-07', 'Cleared', 25),
 (123456, 0007, 11100004, 1004, 'Expired Registration', 2000.00, '2025-09-12', 'Unpaid', NULL),
 (123453, 0004, 11100008, 1008, 'No Seatbelt', 1000.00, '2025-01-29', 'Unpaid', NULL),
 (123453, 0004, 11100008, 1008, 'Unregistered Motor Vehicle', 10000.00, '2025-07-31', 'Unpaid', NULL),
