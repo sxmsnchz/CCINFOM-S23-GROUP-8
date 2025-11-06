2db679f1bbd1ff71f8a2695facdd4cd852e13fdd
Samantha Breanne Sanchez <127837816+sxmsnchz@users.noreply.github.com>
2025-11-07 04:01:30 +0800

    implemented violations related stuff

--- DBApp/src/view/UserMenu.java
+++ DBApp/src/view/UserMenu.java
@@ -1,8 +1,10 @@
 package view;
 
+import java.util.List;
 import java.util.Scanner;
 import service.PaymentService;
 import service.ReceiptService;
+import service.ViolationService;
 import model.Session;
 
 /**
@@ -45,7 +47,37 @@ public class UserMenu {
                     System.out.println("\n[Feature: View Vehicles] (to be implemented)\n");
                     break;
                 case "4":
-                    System.out.println("\n[Feature: View Violations] (to be implemented)\n");
+                    System.out.println("All Violations");
+                    try {
+                        int ownerId = Session.loggedInOwnerId;
+                        ViolationService vs = new ViolationService();
+                        List<model.Violation> violations = vs.getViolationsOfOwner(ownerId);
+
+                        if (violations.isEmpty()) {
+                            System.out.println("No violations found.");
+                            break;
+                        }
+                        System.out.printf("%-4s %-8s %-9s %-8s %-10s %-22s %-9s %-12s%n",
+                                          "ID", "Vehicle", "Owner", "Branch", "Officer", "Type", "Fine", "Date", "Status");
+                        System.out.println("---------------------------------------------------------------------------------------------");
+
+                        for(model.Violation v : violations) {
+                            System.out.printf("%-4s %-8s %-9s %-8s %-10s %-22s %-9s %-12s%n",
+                                              v.getViolationId(),
+                                              v.getVehicleId(),
+                                              v.getOwnerId(),
+                                              v.getBranchId(),
+                                              v.getOfficerId(),
+                                              truncate(v.getViolationType(), 20),
+                                              v.getFineAmount(),
+                                              v.getViolationDate(),
+                                              v.getPaymentStatus());
+                        }
+                        System.out.println();
+                    } catch (Exception e) {
+                        System.out.println("Error loading violations: " + e.getMessage());
+                        e.printStackTrace();
+                    }
                     break;
                 case "5":
                     PaymentService paymentService = new PaymentService();
@@ -71,6 +103,14 @@ public class UserMenu {
             }
         }
 
+
+
         // note: do not close System.in-scanner here to avoid closing System.in for callers
+    }
+
+     private static String truncate(String s, int max) {
+        if (s == null) 
+            return "";
+        return s.length() <= max ? s : s.substring(0, max - 3) + "...";
     }
 }
