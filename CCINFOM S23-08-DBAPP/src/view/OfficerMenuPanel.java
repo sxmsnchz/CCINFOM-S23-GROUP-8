2ac05b35dd53b8956c214db017d7352e6b793a0f
ayamerp <hayilaaiv@gmail.com>
2025-11-19 11:27:24 +0800

    Update OfficerMenuPanel.java
    - fixed missing viewVehicleList panel when click vehicle list button

--- CCINFOM S23-08-DBAPP/src/view/OfficerMenuPanel.java
+++ CCINFOM S23-08-DBAPP/src/view/OfficerMenuPanel.java
@@ -1,7 +1,7 @@
 package view;
 
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class OfficerMenuPanel extends JPanel {
 
@@ -83,7 +83,7 @@ public class OfficerMenuPanel extends JPanel {
         card.add(Box.createVerticalStrut(8));
         card.add(createMenuButton("View Owner List", e -> mainFrame.showOwnerList()));
         card.add(Box.createVerticalStrut(8));
-        card.add(createMenuButton("View Vehicle List", e -> showNotImplemented("View Vehicle List")));
+        card.add(createMenuButton("View Vehicle List", e -> mainFrame.showVehicleList()));
         card.add(Box.createVerticalStrut(8));
         card.add(createMenuButton("View Branch List", e -> mainFrame.showBranchList()));
         card.add(Box.createVerticalStrut(8));
